3d5a8a0608b15e92e68f58a806141fca22271f60
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-11-27 19:40:06 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 import sys
 from git import Repo
 repo = Repo.clone_from('https://github.com/raghug-source/sampleapp.git','/home/circleci/project/raghu/',branch='new-branch')
-#command = "zowe zos-files upload file-to-data-set '/home/circleci/project/raghu/sampleapp/cobol/SAM1.cbl' 'RGOPALK.RAG.COBOL(SAM5)'"
+command = "zowe zos-files upload file-to-data-set '/home/circleci/project/raghu/cobol/SAM1.cbl' 'RGOPALK.RAG.COBOL(SAM5)'"
 # command = "zowe zos-jobs submit data-set 'RGOPALK.JCL.CNTL(HELLO)' --view-all-spool-content"
 # #command = "zowe --version"
-#os.system(command)
+os.system(command)
