073d4536ff6098ddbfc4ffd874762db7ff734083
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-11-27 17:48:39 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 import os
 import sys
-command = "zowe zos-jobs submit data-set 'RGOPALK.JCL.CNTL(HELLO)' --view-all-spool-content"
-#command = "zowe --version"
-os.system(command)
+from git import Repo
+repo = Repo.clone_from('https://github.com/raghug-source/sampleapp.git',branch=new-branch')
+# command = "zowe zos-jobs submit data-set 'RGOPALK.JCL.CNTL(HELLO)' --view-all-spool-content"
+# #command = "zowe --version"
+# os.system(command)
