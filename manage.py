780c825af1e31475bf524c7a9f495fa192f9d81c
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-11-27 13:53:45 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,9 @@ import os
 import sys
 from zowe.zos_console_for_zowe_sdk import Console
 print('Hello World!')
-host = sys.argv[0]
-port = sys.argv[1]
-print(host)
-print(port)
+host = sys.argv[1]
+port = sys.argv[2]
+user = sys.argv[3]
+pwd = sys.argv[4]
+command = "zowe --version"
+os.system(command)
