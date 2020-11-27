5436df1a7bebdc04b5c1cf8cd428576bb85750fe
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-11-27 15:56:45 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,5 @@
 import os
 import sys
-from zowe.zos_console_for_zowe_sdk import Console
-print('Hello World!')
-host = sys.argv[1]
-port = sys.argv[2]
-user = sys.argv[3]
-pwd = sys.argv[4]
+#command = "zowe zos-jobs submit data-set 'RGOPALK.JCL.CNTL(HELLO)' --view-all-spool-content"
 command = "zowe --version"
 os.system(command)
