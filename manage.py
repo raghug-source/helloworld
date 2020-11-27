09c45d1e3c3d1f95a0d1a51fd357f00e443f3ed0
raghug-source <74408008+raghug-source@users.noreply.github.com>
2020-11-27 16:02:08 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 import os
 import sys
-#command = "zowe zos-jobs submit data-set 'RGOPALK.JCL.CNTL(HELLO)' --view-all-spool-content"
-command = "zowe --version"
+command = "zowe zos-jobs submit data-set 'RGOPALK.JCL.CNTL(HELLO)' --view-all-spool-content"
+#command = "zowe --version"
 os.system(command)
